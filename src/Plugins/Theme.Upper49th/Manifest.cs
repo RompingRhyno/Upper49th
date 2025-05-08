0df1641758ba8233ab2c476627af434523dde677
Kohei Dunnet <kdunnet1@my.bcit.ca>
2025-05-08 16:12:16 -0700

    Added a Upper49th Theme to the project

---
+++ src/Plugins/Theme.Upper49th/Manifest.cs
@@ -0,0 +1,9 @@
+﻿using Grand.Infrastructure.Plugins;
+
+[assembly: PluginInfo(
+    FriendlyName = "Upper49th",
+    Group = "Themes",
+    SystemName = "Theme.Upper49th",
+    Author = "Bit by Bit",
+    Version = "1.0.0"
+)]
---
+++ src/Plugins/Theme.Upper49th/Upper49thThemeView.cs
@@ -0,0 +1,22 @@
+﻿using Grand.Web.Common.Themes;
+
+namespace Theme.Upper49th;
+
+public class Upper49thThemeView : IThemeView
+{
+    public string AreaName => "";
+    public string ThemeName => "Upper49th";
+
+    public ThemeInfo ThemeInfo => new("Upper49th", "~/Plugins/Theme.Upper49th/Content/theme.jpg",
+        "Upper49th", false);
+
+    public IEnumerable<string> GetViewLocations()
+    {
+        return new List<string> {
+            "/Views/Upper49th/{1}/{0}.cshtml",
+            "/Views/Upper49th/Shared/{0}.cshtml",
+            "/Views/{1}/{0}.cshtml",
+            "/Views/Shared/{0}.cshtml"
+        };
+    }
+}
