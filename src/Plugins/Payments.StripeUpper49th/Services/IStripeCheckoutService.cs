79ef8b13dddef9fc7d08820091fe610f0e197012
Ryan Jiang <ryan94j@gmail.com>
2025-05-18 09:25:37 -0700

    Created copy of stripe plugin and began customization

---
+++ src/Plugins/Payments.StripeUpper49th/Services/IStripeCheckoutService.cs
@@ -0,0 +1,9 @@
+﻿using Grand.Domain.Orders;
+
+namespace Payments.StripeUpper49th.Services;
+
+public interface IStripeCheckoutService
+{
+    Task<string> CreateRedirectUrl(Order order);
+    Task<bool> WebHookProcessPayment(string stripeSignature, string json);
+}
---
+++ src/Plugins/Payments.StripeUpper49th/StartupApplication.cs
@@ -0,0 +1,27 @@
+using Grand.Business.Core.Interfaces.Checkout.Payments;
+using Grand.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Payments.StripeUpper49th.Services;
+
+namespace Payments.StripeUpper49th;
+
+public class StartupApplication : IStartupApplication
+{
+    public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IPaymentProvider, StripeCheckoutPaymentProvider>();
+        services.AddScoped<IStripeCheckoutService, StripeCheckoutService>();
+        services.AddScoped<IRepository<StripeCustomerLink>, MongoRepository<StripeCustomerLink>>();
+    }
+
+    public int Priority => 10;
+
+    public void Configure(WebApplication application, IWebHostEnvironment webHostEnvironment)
+    {
+    }
+
+    public bool BeforeConfigure => false;
+}
