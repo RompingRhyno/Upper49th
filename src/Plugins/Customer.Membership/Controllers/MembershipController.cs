46be245182e24e8f1626d54af969445cb68ea09f
Joe Lin <jlin249@my.bcit.ca>
2025-05-20 02:59:57 -0700

    Added descriptions to membership plan config interfaces, modified css for membership signup, and added redirection for if a user already has a membership

--- src/Plugins/Customer.Membership/Controllers/MembershipController.cs
+++ src/Plugins/Customer.Membership/Controllers/MembershipController.cs
@@ -93,14 +93,31 @@ namespace Customer.Membership.Controllers
         }
 
         [HttpGet("", Name = "MembershipIndex")]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            if (await hasMemberRole())
+            {
+                return RedirectToAction("MembershipInfo");
+            }
+            else if (!(await _groupService.IsRegistered(_workContext.CurrentCustomer)))
+            {
+                return RedirectToRoute("Login");
+            }
             return View("~/Views/Membership/Index.cshtml");
         }
 
         [HttpGet("signup")]
         public async Task<IActionResult> SignUp()
         {
+            if (await hasMemberRole())
+            {
+                return RedirectToAction("MembershipInfo");
+            }
+            else if (!(await _groupService.IsRegistered(_workContext.CurrentCustomer)))
+            {
+                return RedirectToRoute("Login");
+            }
+
             await _customerService.ResetCheckoutData(_workContext.CurrentCustomer, _storeContext.CurrentStore.Id);
 
             var model = new PlanSelectionModel
@@ -115,6 +132,15 @@ namespace Customer.Membership.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> SignUp(MembershipWizardModel model, string navigation)
         {
+            if (await hasMemberRole())
+            {
+                return RedirectToAction("MembershipInfo");
+            }
+            else if (!(await _groupService.IsRegistered(_workContext.CurrentCustomer)))
+            {
+                return RedirectToRoute("Login");
+            }
+
             // Back navigation
             if (navigation == "back")
             {
@@ -175,7 +201,7 @@ namespace Customer.Membership.Controllers
                     return View("~/Views/Membership/SignUp.cshtml", planModel);
                 }
 
-                //Move to step 2
+                // Move to step 2
                 var billingModel = new BillingAddressModel
                 {
                     CurrentStep = 2,
@@ -227,8 +253,6 @@ namespace Customer.Membership.Controllers
 
                     return View("~/Views/Membership/SignUp.cshtml", billingModel);
                 }
-
-                // TODO: Add validation to make sure all fields are filled out before proceeding and modifying database with blanks
 
                 var addressModel = billingModel.BillingAddress;
                 var address = new Address
@@ -328,9 +352,6 @@ namespace Customer.Membership.Controllers
 
                         };
 
-                        _logger.LogWarning("Email", newOrder.CustomerEmail);
-
-
                         newOrder.OrderTags.Add(model.SelectedPlan);
 
                         await _orderService.InsertOrder(newOrder);
@@ -381,6 +402,11 @@ namespace Customer.Membership.Controllers
         [HttpGet("paymentsuccess")]
         public async Task<IActionResult> PaymentSuccess(string orderId)
         {
+            if (!(await _groupService.IsRegistered(_workContext.CurrentCustomer)))
+            {
+                return RedirectToRoute("Login");
+            }
+
             if (string.IsNullOrEmpty(orderId))
                 return BadRequest("Missing order ID.");
 
@@ -409,6 +435,11 @@ namespace Customer.Membership.Controllers
         [HttpGet("paymentcancel")]
         public async Task<IActionResult> PaymentCancel(string orderId)
         {
+            if (!(await _groupService.IsRegistered(_workContext.CurrentCustomer)))
+            {
+                return RedirectToRoute("Login");
+            }
+
             if (string.IsNullOrEmpty(orderId))
                 return BadRequest("Missing order ID.");
 
@@ -487,11 +518,12 @@ namespace Customer.Membership.Controllers
         private async Task<List<MembershipPlan>> LoadAvailablePlansAsync()
         {
             var settings = await _settingService.LoadSetting<MembershipSettings>();
-            return settings.Plans.Select(p => new MembershipPlan
+            return settings.Plans.OrderBy(p => p.Price).Select(p => new MembershipPlan
             {
                 Role = p.Role,
                 Price = p.Price,
-                SystemName = p.SystemName
+                SystemName = p.SystemName,
+                Description = p.Description
             }).ToList();
         }
 
@@ -522,7 +554,10 @@ namespace Customer.Membership.Controllers
                 FilterByCountryId = filterByCountryId
             });
 
-            return model.PaymentMethods.ToList();
+            // Filter for stripe only currently, other payment methods are not working
+            // return model.PaymentMethods.ToList();
+
+            return model.PaymentMethods.Where(p => p.PaymentMethodSystemName.Equals("Payments.StripeCheckout")).ToList();
         }
 
         // Helper function to get plan role string
@@ -560,6 +595,7 @@ namespace Customer.Membership.Controllers
             return (int)(ticks % int.MaxValue);
         }
 
+        // Helper function to generate a random unique order code
         private async Task<string> GenerateUniqueOrdeCodeAsync()
         {
             const int length = 8;
@@ -574,6 +610,24 @@ namespace Customer.Membership.Controllers
             while ((await _orderService.GetOrdersByCode(code)).Any());
 
             return code;
+        }
+
+        // Helper function to check if customer has any membership role
+        private async Task<bool> hasMemberRole()
+        {
+            // Get all current customer's groups
+            var groupIds = _workContext.CurrentCustomer.Groups.ToList().ToArray();
+            var groups = await _groupService.GetAllByIds(groupIds);
+
+            var availablePlans = await LoadAvailablePlansAsync();
+
+            var groupSystemNames = groups.Select(g => g.SystemName);
+            var planSystemNames = availablePlans.Select(p => p.SystemName);
+
+            var hasCommonSystemName = groupSystemNames.Intersect(planSystemNames).Any();
+
+            return hasCommonSystemName;
+
         }
     }
 }
